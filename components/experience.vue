6bb1bead6ec9ce4d5b68137d76eddb95199603bb
OverRevvv <rdude1612@gmail.com>
2023-07-31 17:03:08 +0530

    fixed another image importing issue

--- components/experience.vue
+++ components/experience.vue
@@ -38,7 +38,7 @@ function  timePosition(itemId:number) {
             <div class="timeline-div">
                 <!-- <div  class="w-10 h-10  rounded-full translate-y-5 border-4 bg-[#17132b] border-[#17132b] p-1" > -->
                 <div  class="w-10 h-10  rounded-full translate-y-52 border-4 bg-[#17132b] border-[#17132b] p-1" >
-                    <img src="images/experience/gc.svg" alt="Arcade">
+                    <img src="/images/experience/gc.svg" alt="Arcade">
                 </div>
                 <!-- <div class="w-10 h-10 rounded-full translate-y-[28rem] border-4 bg-[#17132b] border-[#17132b] p-1" > -->
                 <div class="w-10 h-10 rounded-full translate-y-[40rem] border-4 bg-[#17132b] border-[#17132b] p-1" >
