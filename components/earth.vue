3078b9b97e57de26ccd7032dc6cc2fc403226979
OverRevvv <rdude1612@gmail.com>
2023-08-04 14:11:16 +0530

    preloader for pc 3D model

--- components/earth.vue
+++ components/earth.vue
@@ -1,5 +1,4 @@
 <template>
-  <Loading3D/>
   <TresCanvas alpha>
     <TresPerspectiveCamera :position="[0, 0, 4]" />
     <OrbitControls :enableZoom="false" :enableDamping="true" :autoRotate="true" :autoRotateSpeed="6" :maxPolarAngle="Math.PI / 2" :minPolarAngle="Math.PI / 2" />
